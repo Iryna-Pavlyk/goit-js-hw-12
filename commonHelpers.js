d5c3dfee267cdfc580cc2cb20be5efd1814f02d5
Iryna-Pavlyk <145845654+Iryna-Pavlyk@users.noreply.github.com>
2024-03-01 09:31:09 +0000

    Deploying to gh-pages from @ Iryna-Pavlyk/goit-js-hw-13@9d5ead354b60cab8efbb9a32ce4fad96f07529e6 🚀

---
+++ commonHelpers.js
@@ -0,0 +1,25 @@
+import{S as f,a as h,i as y}from"./assets/vendor-5401a4b0.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))l(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&l(a)}).observe(document,{childList:!0,subtree:!0});function c(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function l(e){if(e.ep)return;e.ep=!0;const t=c(e);fetch(e.href,t)}})();const x="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAEZSURBVHgBrZVRDoMgDIar0fi6m2xH2Y3Ek2w3cFfZXXwBljFQCai0xemfoCaU/xNpKwAirXWrlDY5w8ViPhVmbgwIO14A3zeQKq/2IuwaqOu6A07+zaVUT8iUi+V28rd5NuSIOQs5wxyFUOZKqRtnmIpZQCZz3acWcgcXUjkF0b2bmwFSpAxskMAgYecaWSsFC8AgnHkMqIBR09TCGrnHsZic5iLs3By3ngWsITMgy9yphEyVJZjUM6esHUS9yfea7N7DAmJz/1niM2EhVBZR2UKl8DS/SFO80OhUnCBkoYWgz2MdNAx8q0jFOK/Fy1GQvdqYe50BQc3PgLDmRyCU+aYOfFsoCtPa+yXvp2/ue9rHqLATflCf5QflVUcNIrzeSwAAAABJRU5ErkJggg==";function w(r){let s=r.hits.map(({webformatURL:c,largeImageURL:l,tags:e,likes:t,views:a,comments:A,downloads:d})=>`<li class="gallery-item">
+      <div class="image-wrapper"><a href="${l}"><img class="image" src="${c}" alt="${e}" width="360"
+                    height="200"></a></div>
+      <div class="text-wrapper">
+        <ul class="text-list">
+          <li class="text-list-item">
+            <h3 class="text-list-title">Likes</h3>
+            <p class="text-list-param">${t}</p>
+          </li>
+          <li class="text-list-item">
+            <h3 class="text-list-title">Views</h3>
+            <p class="text-list-param">${a}</p>
+          </li>
+          <li class="text-list-item">
+            <h3 class="text-list-title">Comments</h3>
+            <p class="text-list-param">${A}</p>
+          </li>
+          <li class="text-list-item">
+            <h3 class="text-list-title">Downloads</h3>
+            <p class="text-list-param">${d}</p>
+          </li>
+        </ul>
+      </div>
+    </li>`).join("");return p.classList.remove("is-hidden"),m.insertAdjacentHTML("beforeend",s)}const S={captionsData:"alt",captionDelay:250},U=new f(".image-gallery a",S);async function u(r){await h.get(r).then(({data:s})=>(s.hits.length===0&&(y.error({message:"Sorry, there are no images matching your search query. Please try again!",messageColor:"#fafafb",backgroundColor:"#ef4040",theme:"dark",iconUrl:x,iconColor:"#fafafb",messageSize:"16",closeOnEscape:!0,maxWidth:"432",position:"topRight"}),m.innerHTML=""),w(s),U.refresh())).catch(s=>{console.log(s)})}const L=document.querySelector(".form"),b=document.querySelector("input[name=message]"),m=document.querySelector(".image-gallery"),p=document.querySelector(".load-btn");document.querySelector(".loader");const n={key:"42476589-1f9c9241784e75b96bc1ad3a5",q:"",image_type:"photo",orientation:"horizontal",safesearch:!0,per_page:15};let g="https://pixabay.com/api/?",i="",o;L.addEventListener("submit",r=>{r.preventDefault(),m.innerHTML=null,n.q=b.value,o=1,i=g+new URLSearchParams(n)+`&page=${o}`,console.log(i),u(i)});p.addEventListener("click",()=>{o+=1,i=g+new URLSearchParams(n)+`&page=${o}`,u(i)});
+//# sourceMappingURL=commonHelpers.js.map
